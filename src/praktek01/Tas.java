2eca84e4cda3ce7b03d7cb05ad883c2de3a60a81
Laila_Agustina <lailaagustina025@gmail.com>
2018-03-26 02:06:23 -0700

    Membuat Atribut dan Behavior

--- src/praktek01/Tas.java
+++ src/praktek01/Tas.java
@@ -10,5 +10,19 @@ package praktek01;
  * @author Laila Agustina
  */
 public class Tas {
+    String ukuran;
+    String merk;
+    String model;
+    String bahan;
+    String warna;
     
+    void cetakInfo(){
+        System.out.println("ukuran :"+ukuran);
+        System.out.println("merk   :"+merk);
+        System.out.println("model  :"+ukuran);
+        System.out.println("bahan  :"+bahan);
+        System.out.println("warna  :"+warna);
+        
+        
+    }
 }
